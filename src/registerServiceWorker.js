ee9383fdf47acd8fbf4ddbc83edab9723af792c3
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-01-02 15:03:26 +0700

    Update registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -8,7 +8,7 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
-var homepage = "https://dadangdut33.github.io/";
+var homepage = "https://dadangdut33.github.io";
 
 const isLocalhost = Boolean(
 	window.location.hostname === "localhost" ||
